a79ad7b76454c96490f428e58838502bd4a0cac3
Koert Kuipers <koert@tresata.com>
2015-03-03 17:14:29 -0500

    update build and spark dependency

--- project/Build.scala
+++ project/Build.scala
@@ -1,26 +1,24 @@
 import sbt._
 import sbt.Keys._
 import net.virtualvoid.sbt.graph.Plugin._
-import sbtassembly.Plugin._
-import sbtassembly.Plugin.AssemblyKeys._
 
 object ProjectBuild extends Build {
   lazy val project = Project(
     id = "root",
     base = file("."),
-    settings = Project.defaultSettings ++ graphSettings ++ assemblySettings ++ Seq(
+    settings = Project.defaultSettings ++ graphSettings ++ Seq(
       organization := "com.tresata",
       name := "spark-columnar",
       version := "0.1-SNAPSHOT",
       scalaVersion := "2.10.4",
-      retrievePattern := "[artifact](-[revision])(-[classifier]).[ext]",
-      retrieveManaged := true,
+      crossScalaVersions := Seq("2.10.4", "2.11.5"),
+      libraryDependencies <++= (scalaVersion) {
+        case "2.10.4" => Seq("com.chuusai" % "shapeless_2.10.4" % "2.0.0")
+        case "2.11.5" => Seq("com.chuusai" %% "shapeless" % "2.0.0")
+      },
       libraryDependencies ++= Seq(
-        "com.chuusai" % "shapeless_2.10.4" % "2.0.0",
-        "org.apache.spark" %% "spark-core" % "1.0.0" % "provided",
-        //"org.slf4j" % "slf4j-api" % "1.6.6" % "provided",
-        //"org.slf4j" % "slf4j-log4j12" % "1.6.6" % "test",
-        "org.scalatest" %% "scalatest" % "1.9.2" % "test"
+        "org.apache.spark" %% "spark-core" % "1.2.0" % "provided",
+        "org.scalatest" %% "scalatest" % "2.2.1" % "test"
       )
     )
   )
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,2 @@
 addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.4")
 
-addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.10.2")
